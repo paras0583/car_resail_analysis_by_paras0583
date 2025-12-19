bdc3c6fabe67bdf8036098280853d1c1204fdc44
paras <shobhanautiyal0@gmail.com>
2025-12-19 22:07:10 +0530

    utils module created frequently used function like load save and eval model

---
+++ src/utils.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import dill
+import pickle
+
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.exception import CustomException
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj, file_obj)
+     
+    except Exception as e:
+      CustomException(e, sys)
+
+def evaluate_models(x_train,y_train,x_test,y_test,models,params):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = list(param.values())[i]
+
+            gs=GridSearchCV(model,param,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
+            # now we can compute the predicted values
+
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            # now we can compute the r2 error
+
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(model.keys())[i]]=test_model_score
+
+        return report
+
+    except Exception as e:
+        CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path ,"rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        CustomException(e,sys)
